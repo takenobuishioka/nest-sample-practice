2f027ac84c27752d1c1e7a012246ec9fb8515c6b
takenobuishioka <soramame588@gmail.com>
2022-12-14 09:17:14 +0900

    usersエンドポイントのGETとPOSTを作成

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
   @Post()
   create(@Body() createUser: CreateUserDto) {
-    return createUser;
+    return this.usersService.create(createUser);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: CreateUserDto[] = [];
+  create(user: CreateUserDto) {
+    this.users.push(user);
+  }
+  findAll() {
+    return this.users;
+  }
+}
